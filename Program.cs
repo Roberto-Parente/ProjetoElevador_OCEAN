241955ba501ae1a9da9b5b12fd6dc7ddf0793cbf
Roberto-Parente <105324137+Roberto-Parente@users.noreply.github.com>
2022-05-31 02:26:52 -0400

    Adcionando arquivo inicial Projeto_elevador OCEAN

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ProjetoElevador.Controllers;
+
+namespace ProjetoElevador
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ElevadorController controller = new ElevadorController();
+
+            controller.MostrarTela();
+           
+
+        }
+    }
+}
